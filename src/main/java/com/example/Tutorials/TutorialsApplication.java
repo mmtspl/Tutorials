66be15a7a872a0b5e9a1272d9627680ca9f973b1
mithunmajumdar <mithun.majumdar@gmail.com>
2022-06-18 21:30:21 +0530

    New changes

--- src/main/java/com/example/Tutorials/TutorialsApplication.java
+++ src/main/java/com/example/Tutorials/TutorialsApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TutorialsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TutorialsApplication.class, args);
+
+		System.out.println("\n\nHello Jenkins..\n\n");
 	}
+
 
 }
